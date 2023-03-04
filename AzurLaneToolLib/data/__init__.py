1573faa6cff43761f74c236bfab599e0112b4fd2
thedayofthedoctor <thedayofthedo@gmail.com>
2023-03-04 14:07:40 +0800

    Revert "code Ver.0.6.1β1"
    This reverts commit a2077c081473f63857c7c15cf7a05921a8df2ab4.

--- AzurLaneToolLib/data/__init__.py
+++ AzurLaneToolLib/data/__init__.py
@@ -7,8 +7,8 @@ __init__.py - The core part of the Azur Lane Tool.
 
 Author: Matt Belfast Brown
 Create Date: 2019-07-11
-Version Date: 2022-10-20
-Version: 0.6.0
+Version Date: 2023-03-04
+Version: 0.6.1β1
 
 THIS PROGRAM IS FREE FOR EVERYONE,IS LICENSED UNDER GPL-3.0
 YOU SHOULD HAVE RECEIVED A COPY OF GPL-3.0 LICENSE.
@@ -21,8 +21,8 @@ You should have received a copy of the GNU General Public License along with thi
 """
 
 __title__ = 'AzurLaneToolLib.data'
-__version__ = '0.6.0'
+__version__ = '0.6.1β1'
 __author__ = 'Matt Belfast Brown'
 __license__ = 'GPL-3.0'
-__copyright__ = 'Copyright (c) 2020-2022 Matt Belfast Brown'
+__copyright__ = 'Copyright (c) 2019-2022 Matt Belfast Brown'
 __all__ = ['data_AZR_Lan']
--- AzurLaneToolLib/mode/mode_KSN_Com.py
+++ AzurLaneToolLib/mode/mode_KSN_Com.py
@@ -7,16 +7,16 @@ mode_KSN_Com.py - The core mode of the Azur Lane Tool.
 
 Author: Matt Belfast Brown
 Create Date: 2019-07-11
-Version Date: 2022-10-20
-Version: 0.6.0
+Version Date: 2023-03-04
+Version: 0.6.1β1
 Mode Create Date: 2020-10-07
-Mode Date: 2022-10-20
-Mode Version: 1.1.0
+Mode Date: 2023-03-04
+Mode Version: 1.1.1
 
 THIS PROGRAM IS FREE FOR EVERYONE,IS LICENSED UNDER GPL-3.0
 YOU SHOULD HAVE RECEIVED A COPY OF GPL-3.0 LICENSE.
 
-Copyright (C) 2021-2022 Matt Belfast Brown
+Copyright (C) 2021-2023 Matt Belfast Brown
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
